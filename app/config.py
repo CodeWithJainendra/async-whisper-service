902e724f5348cf1f6a8ddf851e93cb3cacc17002
Jainendra Singh <shivamraaj1@gmail.com>
2026-02-06 00:04:36 +0530

    Refactor: Implement Async Task Queue with Celery and Dockerize

---
+++ app/config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config:
+    CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+    CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
+    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
